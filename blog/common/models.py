ffaf8978de4f777445272202f8f8e6206754437d
Pradip <pradip@Mac114s-MacBook-Pro.local>
2020-02-18 18:44:20 +0530

    new changes

--- blog/common/models.py
+++ blog/common/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
   
     def __str__(self):
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
+from common.models import Category
 # Create your models here.
 class Post(models.Model):
-    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     is_featured = models.BooleanField(default=False)
-
+    categories = models.ManyToManyField(Category)
     def __str__(self):
         return self.title
