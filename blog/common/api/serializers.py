38382a386512136ad2a01b99660d9f595450f4cb
Pradip <pradip@Mac114s-MacBook-Pro.local>
2020-02-19 11:26:56 +0530

    new changes

--- blog/common/api/serializers.py
+++ blog/common/api/serializers.py
@@ -5,3 +5,4 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__' # if not declared, 
+        
--- blog/common/api/views.py
+++ blog/common/api/views.py
@@ -11,39 +11,10 @@ class CategoryListView(generics.ListAPIView):
 class CategoryCreateView(generics.CreateAPIView):
     queryset = Category.objects.all()
     serializer_class = serializers.CategorySerializer
-    def create(self, request, *args, **kwargs):
-        super(CategoryCreateView, self).create(request, args, kwargs)
-        response = {"status_code": status.HTTP_200_OK,
-                    "message": "Successfully created",
-                    "result": request.data}
-        return Response(response)  
+    
         
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = serializers.CategorySerializer
-    def retrieve(self,request,*args , **kwargs):
-        super(CategoryDetailView,self).retrieve(request,args,kwargs)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        data = serializer.data
-        response = {"status_code": status.HTTP_200_OK,
-                    "message": "Successfully retrieved",
-                    "result": data}
-        return Response(response)
-
-    def patch(self, request, *args, **kwargs):
-        super(CategoryDetailView, self).patch(request, args, kwargs)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        data = serializer.data
-        response = {"status_code": status.HTTP_200_OK,
-                    "message": "Successfully updated",
-                    "result": data}
-        return Response(response) 
-
-    def delete(self, request, *args, **kwargs):
-        super(CategoryDetailView, self).delete(request, args, kwargs)
-        response = {"status_code": status.HTTP_200_OK,
-                    "message": "Successfully deleted"}
-        return Response(response)   
+     
 # Create your views here.
